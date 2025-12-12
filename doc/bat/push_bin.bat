7718cbe9cc99ac27357723eb75fb5b16713ed0b9
Nguyen Manh Dung <Dung.NM213838@sis.hust.edu.vn>
2025-12-12 11:21:22 +0700

    bat

--- doc/bat/push_bin.bat
+++ doc/bat/push_bin.bat
@@ -1,14 +1,25 @@
 @echo off
+setlocal
+
+REM ====== THÔNG TIN SERVER ======
 set USER=vht
 set HOST=10.61.248.57
 
-REM File trên Windows
+REM ====== FILE GỐC TRÊN WINDOWS ======
 set LOCAL_FILE=C:\data\bin\app.bin
 
-REM Thư mục đích trên server
+REM ====== TÊN FILE MỚI SAU KHI ĐỔI (CHỈ TÊN, KHÔNG ĐƯỜNG DẪN) ======
+set NEW_NAME=app_2025.bin
+
+REM ====== THƯ MỤC ĐÍCH TRÊN SERVER ======
 set REMOTE_DIR=/home/myuser/bin/
 
-echo Uploading %LOCAL_FILE% ...
-scp "%LOCAL_FILE%" %USER%@%HOST%:%REMOTE_DIR%
+REM ====== TẠO FILE TẠM VỚI TÊN MỚI ======
+set TEMP_FILE=%TEMP%\%NEW_NAME%
+copy /y "%LOCAL_FILE%" "%TEMP_FILE%" >nul
+
+echo Uploading "%TEMP_FILE%" as "%NEW_NAME%" ...
+scp "%TEMP_FILE%" %USER%@%HOST%:%REMOTE_DIR%
+
 echo Done!
 pause
